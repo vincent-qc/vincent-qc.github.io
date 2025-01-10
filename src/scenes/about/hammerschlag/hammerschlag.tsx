e0a615e0855a780eaedb62c7dfb6f01f50577b76
Vincent Qi <vincent.qi.2006@gmail.com>
2025-01-09 23:44:54 -0800

    feat: complete hammerschlag

--- src/scenes/about/hammerschlag/hammerschlag.tsx
+++ src/scenes/about/hammerschlag/hammerschlag.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
 import * as THREE from "three";
 
-const Roof = ({
+const RoofMesh = ({
   scale,
   position,
 }: {
@@ -54,7 +54,7 @@ const Roof = ({
   );
 };
 
-const Front = ({
+const FrontMesh = ({
   scale,
   position,
 }: {
@@ -97,32 +97,128 @@ const Front = ({
   );
 };
 
-const Center = ({
+const PillarMesh = ({
+  scale,
+  position,
+  rotation,
+}: {
+  scale: [number, number, number];
+  position: [number, number, number];
+  rotation?: number;
+}) => {
+  const pillar = useMemo(() => {
+    const shape = new THREE.Shape();
+    shape.moveTo(0, 0);
+    shape.arc(0, 0, 4.5, Math.PI / 8, 0, true);
+    shape.arc(-4.5, 0, 3, 0, Math.PI / 8, false);
+    return shape;
+  }, []);
+
+  return (
+    <group
+      scale={scale}
+      position={position}
+      rotation={[Math.PI / 2, 0, rotation || 0]}
+    >
+      <mesh position={[0, 0, -6]} rotation={[0, 0, Math.PI / 16]}>
+        <extrudeGeometry args={[pillar, { depth: 8, bevelEnabled: false }]} />
+        <meshStandardMaterial color="#E7D1B1" />
+      </mesh>
+    </group>
+  );
+};
+
+const CenterMesh = ({
   scale,
   position,
 }: {
   scale: [number, number, number];
   position: [number, number, number];
 }) => {
-  const pillar = useMemo(() => {
-    const shape = new THREE.Shape();
-    shape.moveTo(0, 0);
-    shape.arc(0, 0, 10, 0, Math.PI / 8, false);
-
-    return shape;
-  }, []);
   return (
     <group scale={scale} position={position}>
-      <mesh position={[10, 30, 10]}>
-        <extrudeGeometry args={[pillar, { depth: 2, bevelEnabled: false }]} />
-        <meshStandardMaterial color="#E7D1B1" />
-      </mesh>
+      {/* Tower */}
+      <group>
+        {/* Center column */}
+        <mesh position={[0, 14, 0]}>
+          <cylinderGeometry args={[2, 2, 8]} />
+          <meshStandardMaterial color="#B7A181" />
+        </mesh>
+        {/* Pillars */}
+        <PillarMesh
+          scale={[1, 1, 1]}
+          position={[0, 12, 0]}
+          rotation={-Math.PI / 4}
+        />
+        <PillarMesh
+          scale={[1, 1, 1]}
+          position={[0, 12, 0]}
+          rotation={-Math.PI / 2}
+        />
+        <PillarMesh
+          scale={[1, 1, 1]}
+          position={[0, 12, 0]}
+          rotation={-(3 * Math.PI) / 4}
+        />
+        <PillarMesh scale={[1, 1, 1]} position={[0, 12, 0]} /> {/* E, CW */}
+        <PillarMesh
+          scale={[1, 1, 1]}
+          position={[0, 12, 0]}
+          rotation={Math.PI / 4}
+        />
+        <PillarMesh
+          scale={[1, 1, 1]}
+          position={[0, 12, 0]}
+          rotation={Math.PI / 2}
+        />
+        <PillarMesh
+          scale={[1, 1, 1]}
+          position={[0, 12, 0]}
+          rotation={(3 * Math.PI) / 4}
+        />
+        <PillarMesh
+          scale={[1, 1, 1]}
+          position={[0, 12, 0]}
+          rotation={Math.PI}
+        />
+        {/* Top */}
+        <mesh position={[0, 19, 0]}>
+          <cylinderGeometry args={[5, 5, 2]} />
+          <meshStandardMaterial color="#E7D1B1" />
+        </mesh>
+        <mesh position={[0, 20, 0]}>
+          <cylinderGeometry args={[3, 3, 2]} />
+          <meshStandardMaterial color="#E7D1B1" />
+        </mesh>
+      </group>
+
       <mesh>
         <boxGeometry args={[16, 16, 16]} />
         <meshStandardMaterial color="#E7D1B1" />
       </mesh>
       <mesh position={[0, 9, 0]} rotation={[0, Math.PI / 8, 0]}>
         <cylinderGeometry args={[7, 7, 2, 8]} />
+        <meshStandardMaterial color="#E9D9D1" />
+      </mesh>
+      <mesh position={[0, 10, 0]} rotation={[0, Math.PI / 8, 0]}>
+        <cylinderGeometry args={[5, 5, 2]} />
+        <meshStandardMaterial color="#E0D0D0" />
+      </mesh>
+    </group>
+  );
+};
+
+const WingMesh = ({
+  scale,
+  position,
+}: {
+  scale: [number, number, number];
+  position: [number, number, number];
+}) => {
+  return (
+    <group scale={scale} position={position}>
+      <mesh>
+        <boxGeometry args={[16, 16, 16]} />
         <meshStandardMaterial color="#E7D1B1" />
       </mesh>
     </group>
@@ -140,9 +236,10 @@ export default function HammerschlagMesh({
 }) {
   return (
     <group scale={scale} position={position} rotation={[0, rotation || 0, 0]}>
-      <Roof scale={[1, 1, 1]} position={[0, 12, 5]} />
-      <Front scale={[1, 1, 1]} position={[0, 0, 0]} />
-      <Center scale={[1, 1, 1]} position={[0, 8, -27]} />
+      <RoofMesh scale={[1, 1, 1]} position={[0, 12, 5]} />
+      <FrontMesh scale={[1, 1, 1]} position={[0, 0, 0]} />
+      <CenterMesh scale={[1, 1, 1]} position={[0, 8, -27]} />
+      <WingMesh scale={[1, 1, 1]} position={[18, 8, -27]} />
     </group>
   );
 }
