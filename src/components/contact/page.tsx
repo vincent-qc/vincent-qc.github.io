93f2dc7a209c067163bfd85005b24fbf3dd158a8
Vincent Qi <vincent.qi.2006@gmail.com>
2025-01-13 00:15:41 -0500

    chore: overflow for contact page

--- src/components/contact/page.tsx
+++ src/components/contact/page.tsx
@@ -3,7 +3,12 @@ import { contacts } from "./contacts";
 
 export default function ContactPage() {
   return (
-    <div className="flex h-full w-full flex-col gap-2 py-4">
+    <div
+      className="flex h-full w-full flex-col gap-2 overflow-auto py-4"
+      style={{
+        scrollbarWidth: "none",
+      }}
+    >
       <div className="flex flex-col gap-4">
         {contacts.map((contact, index) => (
           <ContactCard key={index} {...contact} index={index} />
