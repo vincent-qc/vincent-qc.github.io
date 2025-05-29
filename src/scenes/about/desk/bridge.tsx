7f258e589f249e1704cf0a53100fd104097eef97
Vincent Qi <vincent.qi.2006@gmail.com>
2025-05-28 20:54:23 -0700

    feat: towermesh

--- src/scenes/about/desk/bridge.tsx
+++ src/scenes/about/desk/bridge.tsx
@@ -1,5 +1,13 @@
-const BridgeMesh = ({ position }: { position: [number, number, number] }) => {
+const TowerMesh = ({ position }: { position: [number, number, number] }) => {
   return <group position={position}></group>;
+};
+
+const BridgeMesh = ({ position }: { position: [number, number, number] }) => {
+  return (
+    <group position={position}>
+      <TowerMesh position={[1, 1, 1]} />
+    </group>
+  );
 };
 
 export { BridgeMesh };
